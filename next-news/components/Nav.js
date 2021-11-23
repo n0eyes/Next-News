566c315c0df1bac6dda369b45f82bfd2c83677a0
n0eyes <doll4586633@naver.com>
2021-11-23 18:01:39 +0900

    fix: lint 충돌 해결

--- next-news/components/Nav.js
+++ next-news/components/Nav.js
@@ -1,32 +1,29 @@
-import useSWR from "swr";
 import styles from "../styles/Nav.module.css";
-import Link from 'next/link'
+import Link from "next/link";
 function Nav() {
-  return <div className={styles.container}>
-    <span className={styles.nav_logo}>SY</span>
-    <div className={styles.nav_main}>
-      <Link href='/news'>
+  return (
+    <div className={styles.container}>
+      <span className={styles.nav_logo}>SY</span>
+      <div className={styles.nav_main}>
+        <Link href="/news">
           <a className={styles.news}>Hacker News</a>
         </Link>
-      <div className={styles.small_nav}>
-        <Link href='/newest'>
-          <a>new</a>
-        </Link>
-        <Link href='/ask'>
-          <a>ask</a>
-        </Link>
-        <Link href='/show'>
-          <a>show</a>
-        </Link>
-        <Link href='/jobs'>
-          <a>jobs</a>
-        </Link>
+        <div className={styles.small_nav}>
+          <Link href="/newest">
+            <a>new</a>
+          </Link>
+          <Link href="/ask">
+            <a>ask</a>
+          </Link>
+          <Link href="/show">
+            <a>show</a>
+          </Link>
+          <Link href="/jobs">
+            <a>jobs</a>
+          </Link>
+        </div>
       </div>
     </div>
-    
-
-  </div>;
+  );
 }
-//git 오류 찾기
 export default Nav;
-
--- next-news/pages/index.js
+++ next-news/pages/index.js
@@ -1,21 +1,13 @@
 import styles from "../styles/Main.module.css";
 import useSWR from "swr";
 import axios from "axios";
-function getFeeds() {
+
+export default function Main() {
   const { data, error } = useSWR(
     "https://api.hnpwa.com/v0/news/1.json",
     async (url) => await axios.get(url).then((res) => res.data)
   );
-  console.log(data);
-  return {
-    newsFeed: data,
-    error,
-  };
-}
-
-export default function Main() {
-  const { newsFeed, error } = getFeeds();
   if (error) return <div>에러</div>;
-  if (!newsFeed) return <div>로딩중</div>;
+  if (!data) return <div>로딩중</div>;
   return <div className={styles.container}>adsdsadsa</div>;
 }
